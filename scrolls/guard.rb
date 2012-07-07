319d8750f21d36f480dbdee512ba77d2caafce1d
James Conroy-Finn <james@logi.cl>
2012-07-07 17:17:47 +0100

    Update Guard scroll
    - Replace use of RbConfig with the listen gem. Guard now uses this gem
      to monitor file system changes, making use of RbConfig redundant.
    
    - Remove LiveReload guard in favour of using the dedicated LiveReload
      application.
    
    - Rename the Bundler group to guard. The gems in this group don't need
      to be required in development, so let's make sure they aren't.

--- scrolls/guard.rb
+++ scrolls/guard.rb
@@ -1,59 +1,16 @@
-prepend_file 'Gemfile' do <<-RUBY
-require 'rbconfig'
-HOST_OS = RbConfig::CONFIG['host_os']
-
-RUBY
-end
-
-append_file 'Gemfile' do <<-RUBY
-
-guard_notifications = #{config['guard_notifications'].inspect}
-group :development do
-  case HOST_OS
-  when /darwin/i
-    gem 'rb-fsevent'
-    gem 'ruby_gntp' if guard_notifications
-  when /linux/i
-    gem 'libnotify'
-    gem 'rb-inotify'
-  when /mswin|windows/i
-    gem 'rb-fchange'
-    gem 'win32console'
-    gem 'rb-notifu' if guard_notifications
-  end
-end
-RUBY
-end
-
-
-# LiveReload
-
-application nil, :env => "development" do
-  "config.middleware.insert_before(Rack::Lock, Rack::LiveReload)"
-end
-
-gem_group :development do
-  gem 'guard-livereload'
-  gem 'yajl-ruby'
-  gem 'rack-livereload'
-
-# Guard for other Scrolls
-
+gem_group :guard do
+  gem 'listen'
   gem 'guard-bundler'
   gem 'guard-test' if scrolls.include? 'test_unit'
 
-  KNOWN_GUARD_SCROLLS = %w[cucumber haml less passenger puma redis resque rspec spork unicorn]
-  KNOWN_GUARD_SCROLLS.each do |scroll|
+  %w(haml less passenger puma resque unicorn spork rspec cucumber).each do |scroll|
     gem "guard-#{scroll}" if scrolls.include? scroll
   end
 end
 
 after_bundler do
-  run "bundle exec guard init"
-  
-  # TODO move livereload to the top of the Guardfile so it is zippy quick
+  run 'bundle exec guard init'
 end
-
 
 __END__
 
@@ -67,9 +24,3 @@ run_after: []
 run_before: []
 
 category: other
-# exclusive: 
-
-config:
-  - guard_notifications:
-      type: boolean
-      prompt: "Enable desktop Guard/Growl notifications?"
